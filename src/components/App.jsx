46e7a0d6df1a11f0a661bed0eb7b7637a45564ad
Kamil Szafrański <107311803+KamilSzafranski@users.noreply.github.com>
2023-01-31 18:39:15 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,84 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Sheet } from './Sheet/Sheet';
+import { Fragment } from 'react';
+import { Contacts } from './Contacts/Contacts';
+import css from './App.module.css';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  renderFilteredData = () => {
+    return this.state.contacts.filter(element => {
+      return element.name
+        .toLowerCase()
+        .startsWith(this.state.filter.toLowerCase());
+    });
+  };
+
+  inputChange = event => {
+    this.setState((state, props) => {
+      return {
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+
+  deleteItem = event => {
+    const data = this.state.contacts.filter(element => {
+      return element.name !== event.target.dataset.name;
+    });
+    this.setState(state => {
+      return { contacts: data };
+    });
+  };
+
+  addContact = event => {
+    event.preventDefault();
+    const {
+      name: { value: text },
+      number: { value: num },
+    } = event.currentTarget.elements;
+    const nameTaken = this.state.contacts.some(
+      elements => elements.name === text
+    );
+    const numberTaken = this.state.contacts.some(
+      elements => elements.number === num
+    );
+    if (nameTaken && numberTaken) {
+      return alert(`${text} is alredy in Phonebook`);
+    }
+    const objectToAdd = {
+      id: nanoid(),
+      name: text,
+      number: num,
+    };
+    this.setState((state, props) => {
+      return {
+        contacts: [...this.state.contacts, objectToAdd],
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className={css.container}>
+        <Sheet add={this.addContact} />
+        <Contacts
+          data={this.renderFilteredData()}
+          filter={this.state.filter}
+          handler={this.inputChange}
+          deleteItem={this.deleteItem}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,51 @@
+import { element } from 'prop-types';
+import React, { Component } from 'react';
+import css from './Contacts.module.css';
+import PropTypes from 'prop-types';
+
+export class Contacts extends Component {
+  render() {
+    const { data, handler, deleteItem } = this.props;
+    return (
+      <div className={css.box}>
+        <h2 className={css.title}>Contacts</h2>
+        <p className={css.description}>Find contacts by name</p>
+        <input
+          type="text"
+          name="filter"
+          onChange={handler}
+          className={css.input}
+        />
+        <ul>
+          {data.map(element => {
+            return (
+              <li className={css.item} key={element.id}>
+                <span>{element.name}:</span>
+                <span> {element.number}</span>
+                <button
+                  data-name={element.name}
+                  className={css.deleteBtn}
+                  onClick={deleteItem}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+Contacts.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string,
+      number: PropTypes.string,
+    })
+  ).isRequired,
+  handler: PropTypes.func.isRequired,
+  deleteItem: PropTypes.func.isRequired,
+};
